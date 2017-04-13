c64338851700be0e183e8218027dc63b90add1bb
abhijava22 <abhijava22@gmail.com>
2017-04-13 12:52:31 -0400

    Add files via upload

---
+++ ugo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Abhishek
+ *
+ */
+public class ugo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to ugo team");
+
+	}
+
+}
