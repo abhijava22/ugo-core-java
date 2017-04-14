3d9928da9e94bfeb6ee3444c75be49e1454a1f80
abhijava22 <abhijava22@gmail.com>
2017-04-14 10:07:57 -0400

    Add files via upload

---
+++ s1_04_14/Practice.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package s1_04_14;
+
+/**
+ * @author Abhishek
+ *
+ */
+public class Practice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Welcome to Ugo");
+	}
+
+}
