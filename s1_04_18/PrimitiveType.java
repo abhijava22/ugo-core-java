98611211c506fa7f1afdce301a7f7c1ed3cf31b2
abhijava22 <abhijava22@gmail.com>
2017-04-18 23:10:32 -0400

    Add files via upload
    Primitive data types

---
+++ s1_04_18/PrimitiveType.java
@@ -0,0 +1,38 @@
+/*This is when i learned how o create a program with methods and data types */
+package s1_04_18;
+
+/**
+ * @author Abhishek
+ *
+ */
+public class PrimitiveType {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 100;
+		byte b = -50;
+		short s = 10000;
+		short r = -20000;
+		int c = 100000;
+		int d = -200000;
+		long e = 100000L;
+		long f = -200000L;
+		float f1 = 234.5f;
+		double d1 = 123.4;
+		boolean one = true;
+		char letterA = 'A';
+
+		System.out.println("This is primitive data type int :" + a + "\t" + b);
+		System.out.println("This is primitive data type short : " + s + "\t" + r);
+		System.out.println("This is primitive data type int: " + c + "\t" + d);
+		System.out.println("This is primitive data type long: " +e +"\t"+f);
+		System.out.println("This is primitive data type float: "  +f1);
+		System.out.println("This is primitive data type double: "  +d1);
+		System.out.println("This is primitive data type boolean: "  +one);
+		System.out.println("This is primitive data type char: "  +letterA);
+	} 
+
+}
